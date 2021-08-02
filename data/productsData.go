94307e194c2bfa088cb5a870cceb05d89bbf82e2
Bothra <manish.bothra@SW-LP10738.local>
2021-08-02 19:24:17 +0530

    Added json annotations

--- data/productsData.go
+++ data/productsData.go
@@ -4,7 +4,7 @@ import "time"
 
 type Product struct {
 	ID          int     `json:"id"`
-	Name        string  `json:"name`
+	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
 	SKU         string  `json:"sku"`
