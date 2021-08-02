6da9336eb0c89d0082e5e6e46adc65871c2272b3
Bothra <manish.bothra@SW-LP10738.local>
2021-08-02 16:30:13 +0530

    Debugging

--- data/productsData.go
+++ data/productsData.go
@@ -1,38 +1,41 @@
 package data
 
-type Product struct{
-	ID int
-	Name string
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
 	Description string
-	Price float64
-	SKU string
-	CreatedOn string
-	UpdatedOn string
-	DeletedOn string
+	Price       float64
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
 }
 
-func GetProducts() []*Product{
+func GetProducts() []*Product {
 	return productList
 }
+
 var productList = []*Product{
 	&Product{
-		ID: 1,
-		Name: Product1,
-		Description: Product1,
-		Price: 111.11,
-		SKU: SKU1,
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
-		DeletedOn: time.Now().UTC().String(),
+		ID:          1,
+		Name:        "Product1",
+		Description: "Product1",
+		Price:       111.11,
+		SKU:         "SKU1",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+		DeletedOn:   time.Now().UTC().String(),
 	},
 	&Product{
-		ID: 2,
-		Name: "Product2",
-		Description: Product2,
-		Price: 222.22,
-		SKU: SKU2,
-		CreatedOn: time.Now().UTC().String(),
-		UpdatedOn: time.Now().UTC().String(),
-		DeletedOn: time.Now().UTC().String(),
-	}
+		ID:          2,
+		Name:        "Product2",
+		Description: "Product2",
+		Price:       222.22,
+		SKU:         "SKU2",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+		DeletedOn:   time.Now().UTC().String(),
+	},
 }
